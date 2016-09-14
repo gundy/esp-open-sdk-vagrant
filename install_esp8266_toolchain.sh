01aac20fd900762e5454670daff4258be786080f
David Gundersen <gundy@acm.org>
2016-09-14 20:06:19 +1000

    add sming, esptool2

--- install_esp8266_toolchain.sh
+++ install_esp8266_toolchain.sh
@@ -1,4 +1,8 @@
 
+# ==================================================================================
+#  INSTALL pfalcon:esp-open-sdk
+# ==================================================================================
+
 # based on instructions at https://github.com/pfalcon/esp-open-sdk
 # .. and here: https://forum.micropython.org/viewtopic.php?t=1965
 
@@ -25,4 +29,64 @@ PATH=$PATH:/opt/esp-open-sdk/esptool
  
 # grant access to serial port
 sudo adduser vagrant dialout
+
+
+
+
+
+# ==================================================================================
+#  INSTALL SmingHub:Sming
+# ==================================================================================
+
+# prepare for install of sming - install dependencies
+sudo apt-get install -y \
+              make unrar autoconf automake libtool gcc g++ gperf flex bison texinfo \
+              gawk ncurses-dev libexpat1-dev python sed python-serial python-dev \
+              srecord bc git help2man
+
+# create /opt folder
+sudo mkdir -p /opt
+
+# check out the Sming repo from github
+cd /opt
+sudo git clone https://github.com/SmingHub/Sming.git
+sudo chown -R vagrant /opt/Sming
+
+# ----------------------------------------------------------------------------
+#  to checkout stable branch rather than develop, uncomment below two lines
+# ----------------------------------------------------------------------------
+# cd /opt/Sming
+# git checkout origin/master
+# -----------------------------
+
+export ESP_HOME=/opt/esp-open-sdk
+export SMING_HOME=/opt/Sming/Sming
+
+cd $SMING_HOME
+make
+
+
+make spiffy
+
+echo 'export ESP_HOME=/opt/esp-open-sdk' >> ~/.profile
+echo 'export SMING_HOME=/opt/Sming/Sming' >> ~/.profile
+
+
+
+# ==================================================================================
+#  INSTALL raburton:esptool2
+# ==================================================================================
+
+# create /opt folder
+sudo mkdir -p /opt
+
+# check out the Sming repo from github
+cd /opt
+sudo git clone https://github.com/raburton/esptool2.git
+sudo chown -R vagrant /opt/esptool2
+
+cd /opt/esptool2
+make
+
+echo 'PATH=$PATH:/opt/esptool2' >> ~/.profile
 
